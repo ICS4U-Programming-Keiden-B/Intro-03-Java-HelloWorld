caf1a6ee3c85393acd5fe61fb8851cb6d4981b25
Keiden Byrne <keiden.byrne@stu.ocsb.ca>
2023-02-07 19:21:42 +0000

    Day 3: Saying Hi!

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+
+public class HelloWorld {
+   public static void main(String []args) {
+      System.out.println("Hey there, Bucko.");
+      System.out.println("This town ain't big 'nough fer the two o' us.");
+   }
+}
