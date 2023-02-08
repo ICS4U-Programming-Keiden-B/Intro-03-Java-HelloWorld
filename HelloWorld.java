13203aa7ad17018d31d87958ae0cf013ee479aed
Keiden Byrne <keiden.byrne@stu.ocsb.ca>
2023-02-08 15:47:24 +0000

    Fixing super linter errors

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,25 @@
+/**
+* Gives a greeting and an extra line of code.
+*
+* @author  Keiden B
+* @version 1.0
+* @since   2023-02-07
+*/
 
-public class HelloWorld {
-   public static void main(String []args) {
-      System.out.println("Hey there, Bucko.");
-      System.out.println("This town ain't big 'nough fer the two o' us.");
-   }
+public final class HelloWorld {
+    /**
+    * Placeholder function to prevent HideUtilityClass Error.
+    */
+    private void placeHolder() {
+    }
+
+    /**
+    * Main lines of code.
+    *
+    * @param args a placeholder value when making the main function
+    */
+    public static void main(String[] args) {
+        System.out.println("Hey there, Bucko.");
+        System.out.println("This town ain't big 'nough fer the two o' us.");
+    }
 }
